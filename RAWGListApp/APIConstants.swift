e57d9eefaf8aed502bc1d2e764739242d2c5405c
Enes KILIÇ <enes843@gmail.com>
2022-10-31 07:47:25 +0300

    Fix typo on APIConstants.swift

--- RAWGListApp/APIConstants.swift
+++ RAWGListApp/APIConstants.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 struct APIContans {
-    // Get your Rapid API Key on fallowing link
+    // Get your Rapid API Key on the following link
     // https://rapidapi.com/accujazz/api/rawg-video-games-database
     static let rapidApiKey = "Paste_Rapid_API_Key_Here"
     
-    // Get your RAWG API Key on fallowing link
+    // Get your RAWG API Key on the following link
     // https://rawg.io/apidocs
     static let rawgGamesApiKey = "Paste_RAWG_API_Key_Here"
 }
